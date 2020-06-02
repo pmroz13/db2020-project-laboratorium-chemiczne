4864bda6f8fd3e4774db1e4d0e86b1b078ecdb8c
pmroz13 <paulinam260@gmail.com>
2020-06-02 21:26:45 +0200

    tabela sprzet_lab
    dodane dane do testowania aplikacji

--- src/sql/db.sql
+++ src/sql/db.sql
@@ -15,3 +15,16 @@ CREATE TABLE IF NOT EXISTS info_o_zwiazku (
     temp_topnienia float(20),
     masa_molowa float(20)
 );
+
+
+CREATE TABLE IF NOT EXISTS sprzet_lab(
+	id_sprzetu int AUTO_INCREMENT,
+    PRIMARY KEY (id_sprzetu),
+    nazwa varchar(20),
+    ilosc int(20),
+    uwagi varchar(300)
+);
+
+
+INSERT INTO info_o_zwiazku(wzor, nazwa_zwiazku, stan_skupienia, uwagi, cena_za_gram, obecny_stan_w_magazynie, pH, rodzaj_wiazania_w_zwiazku, temp_wrzenia, temp_topnienia, masa_molowa) VALUES('H2O','woda','ciecz','taka fajna woda', '2','100','0','jakies napewno','100','30','2');
+INSERT INTO sprzet_lab(nazwa, ilosc, uwagi) VALUES ('probowka', '20', 'zadnych uwag');
