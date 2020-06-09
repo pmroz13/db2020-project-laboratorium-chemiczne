c9dcccc186cbbd522cd3d20b42d7e84f2e637522
pmroz13 <paulinam260@gmail.com>
2020-06-09 16:25:50 +0200

    baza
    dodanie tabel do bazy danych

--- src/app/labor.py
+++ src/app/labor.py
@@ -1,6 +1,6 @@
-import os
-import sys
-#sys.path.insert(0,"C:\Users\Dell\AppData\Roaming\Python\Python37\site-packages")
+import mysql.connector
+from mysql.connector import errorcode
+import time
 import pymysql
 
 class Pracownik():
@@ -18,7 +18,7 @@ class Pracownik():
         print("Aktualizuj stan magazynu:")
  
     def dodaj_do_listy_zakupow():
-        print("Dodawanie do listy zakupów:")
+        print("Dodawanie do listy zakupow:")
 
     def utworz_konto():
         print("Tworzenie konta:")
@@ -28,13 +28,26 @@ class Administrator(Pracownik):
         super()._init_(id_pracownika)
 
     def nadzor_nad_lista_zakupow():
-        print("Zarządzanie lista zakupów")
+        print("Zarzadzanie lista zakupow")
     def aktualizuj_dane_pracownika():
-        print("Zmień dane pracownika")
+        print("Zmien dane pracownika")
     def wyswietl_wydatki():
         print("Wydatki:")
     def aktualizuj_dane_odbiorcow():
-        print("Aktualizowanie danych odbiorców:")
+        print("Aktualizowanie danych odbiorcow:")
+
+
+
+def polaczenie():
+    connection = pymysql.connect(
+        host="localhost",
+        user="user",
+        password="password",
+        database="db",
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return connection
 
 class Zwiazek_chemiczny():
     def _init_(self,id_zwiazku):
@@ -46,7 +59,7 @@ class Zwiazek_chemiczny():
         def wyswietlInformacjeChemiczne(self, id_zwiazku):
             connection = polaczenie()
             with connection.cursor() as cursor:
-                sql = f"SELECT nazwa_zwaizku, stan_skupienia, rodzaj_wiazania_w_zwiazku, temp_wrzenia, temp_topnienia, masa_molowa, pH, uwagi FROM info_o_zwiazku WHERE info_o_zwiazku.id_zwiazku=id_zwiazku; "
+                sql = f"SELECT nazwa_zwiazku, stan_skupienia, rodzaj_wiazania_w_zwiazku, temp_wrzenia, temp_topnienia, masa_molowa, pH, uwagi FROM info_o_zwiazku WHERE info_o_zwiazku.id_zwiazku=id_zwiazku; "
                 # self.__cursor.execute('SELECT nazwa_zwaizku, stan_skupienia, rodzaj_wiazania_w_zwiazku,'
                 #                     ' temp_wrzenia, temp_topnienia, masa_molowa, pH, uwagi FROM info_o_zwiazku'
                 #                     ' WHERE info_o_zwiazku.id_zwiazku=id_zwiazku')
@@ -90,83 +103,125 @@ class Magazyn():
 
 class Konto():
 
-
     def zaloguj(account):
         print("*****Logowanie*****")
-        nick = input("Wprowadź nick: ")
-        haslo = input("Wprowadź hasło: ")
-        print("Tutaj łączymy się z bazą danych a ona nam zwraca albo błąd, "
-              "albo ID tej osoby.")
-        odpowiedz_bazy = input("Wybierz 1 jesli chcesz BŁĄD lub Wybierz 2 "
-              "jeśli chesz poprawne zalogowanie, 3 poprawne zalogowanie admina: ")
-        if(odpowiedz_bazy=='1'):
-            print("Błędny login lub hasło. Jeśli nie pamiętasz hasła wybierz 1,"
-                  "Jeśli chcesz spróbować ponownie wybierz 2")
+        nick = input("Wprowadz nick: ")
+        haslo = input("Wprowadz haslo: ")
+
+        query = "SELECT haslo FROM pracownicy WHERE login = '{}'".format(nick)
+        cursor.execute(query)
+        haselka=cursor.fetchall()
+        odpowiedz_bazy=1
+        for haselko in haselka:
+            if haselko[0]==haslo:
+                odpowiedz_bazy=2
+                print('22222')
+            else:
+                odpowiedz_bazy=1
+                print('11111')
+
+        if (odpowiedz_bazy == 1):
+            print("Bledny login lub haslo. Jesli nie pamietasz hasla wybierz 1,"
+                  "Jesli chcesz sprobowac ponownie wybierz 2")
             wybor = input("Twoj wybor: ")
-            if(wybor=='1'):
+            if (wybor == '1'):
                 account.resetuj_haslo()
-            elif(wybor=='2'):
+            elif (wybor == '2'):
                 account.zaloguj()
-        elif (odpowiedz_bazy == '2'):
-            print("Po poprawnym zalogowaniu na zwyklego uzytkownika"
-                  "następuje pobranie jego ID i utworzenie obiektu klasy pracownik"
-                  "o tym ID")
-            worker=Pracownik(3)
-           # worker.id_pracownika=4
-        elif (odpowiedz_bazy == '3'):
-            print("Po poprawnym zalogowaniu na admina"
-                  "następuje pobranie jego ID i utworzenie obiektu "
-                  "klasy administrator o tym ID")
-            adm=Administrator()
-            adm.id_pracownika=6
-        else:
+        elif (odpowiedz_bazy == 2):
+            query = "SELECT id_pracownika,admin FROM pracownicy WHERE login = '{}'".format(nick)
+            cursor.execute(query)
+            dane = cursor.fetchall()
+            for (id_pracownika,admin) in dane:
+                if(admin==0):
+                    global worker
+                    worker=Pracownik(id_pracownika)
+                    wybieranie2(id_pracownika,admin)
+                else:
+                    global adm
+                    adm = Administrator(id_pracownika)
+                    wybieranie2(id_pracownika, admin)
+
             return 0
 
-
-
-
-    def zarejestruj(self):
+    def zarejestruj(account):
         print("*****Rejestracja*****")
 
         login = input("Login: ")
         email = input("email: ")
         haslo = input("Hasło: ")
         haslo2 = input("Powtorz haslo:")
-        print("Tu następuje sprawdzenie czy hasla sie zgadzaja, oraz czy mail"
-              "oraz nick nie są już zajęte, jeśli nie przechodzimy do nastepnej"
-              "części rejestracji")
+
+        query = "SELECT login FROM pracownicy WHERE login = '{}'".format(login)
+        cursor.execute(query)
+        nicki = cursor.fetchall()
+        if nicki:
+            print('Podany login jest juz zajety, wybierz inny')
+            account.zarejestruj()
+
 
         imie = input("Imie: ")
         nazwisko = input("Nazwisko: ")
         adres = input("Adres(Kod pocztowy, Miejscowość, Ulica, nr mieszkania): ")
-        numer_konta= input("Numer konta bankowego: ")
+        numer_konta = input("Numer konta bankowego: ")
 
         print("Teraz dodajemy konto do bazy danych")
+        query = "INSERT INTO pracownicy SET imie='{}', nazwisko='{}',adres='{}',numer_konta='{}',login='{}',haslo='{}',\
+                email='{}'".format(imie,nazwisko,adres,numer_konta,login,haslo,email)
+        cursor.execute(query)
+        mydb.commit()
+        print("Pomyślnie utworzono konto!!!")
+        wybieranie1(account)
 
 
 
-    def resetuj_haslo(self):
+    def resetuj_haslo(account):
         print("******Resetowanie hasła******")
-        login = input("Login: ")
-        email = input("email: ")
-        print("Bza danych sprawdza czy taki login i hasło są w bazie,")
-        odpowiedz_bazy = input("Wybierz 1 jesli chcesz BŁĄD lub Wybierz 2 "
-                               "jeśli chesz zgodę na zmianę hasła")
+        email = input("Podaj email: ")
+
+        query = "SELECT email FROM pracownicy WHERE email = '{}'".format(email)
+        cursor.execute(query)
+        emejle = cursor.fetchall()
+        if emejle:
+            print('Na adres '+email+' zostal wyslany KOD, ktory należy wpisać, aby zresetować hasło'
+                  '(domyslny kod to 1234)')
+            kod=input("Wprowadz kod:")
+            if(kod=='1234'):
+                nowehaslo=input("Wprowadz teraz nowe haslo do konta:")
+                nowehaslo2=input("Powtórz hasło:")
+                query = "UPDATE pracownicy SET haslo='{}' WHERE email='{}'".format(nowehaslo,email)
+                cursor.execute(query)
+                mydb.commit()
+            else:
+                print('BŁĘDNY KOD! Powrót do MENU')
+                wybieranie1(account)
+
+
+        else:
+            print("Podanego adreu email nie ma w naszej bazie")
+            wybieranie1(account)
+
+
+
 
 
 #/////////////////////Funkcje/////////////////
 def main():
+    # odczyt
+    global mydb
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd="", database="laboratorium")
+    global cursor
+    cursor = mydb.cursor()
     account = Konto()
     wybieranie1(account)
 
 
 
-
 def wybieranie1(account):
     print("Witamy w laboratorium chemicznym\n1.Zaloguj"
-          "\n2.Zarejestruj.\n3.Zapomniałem hasła."
+          "\n2.Zarejestruj.\n3.Zapomnialem hasla."
           "\n4.Zamknij")
-    wybor = input("Wprowadź wartość: ")
+    wybor = input("Wprowadz wartosc: ")
     if (wybor == '1'):
         account.zaloguj()
     elif (wybor == '2'):
@@ -176,8 +231,16 @@ def wybieranie1(account):
     elif (wybor == '4'):
         return 0
     else:
-        print("Wybrano zla wartosc, WYBIERZ OPCJĘ OD 1 do 4:")
+        print("Wybrano zla wartosc, WYBIERZ OPCJE OD 1 do 4:")
         wybieranie1()
+
+
+def wybieranie2(id_pracownika, admin):
+    query = "SELECT imie,nazwisko FROM pracownicy WHERE id_pracownika = {}".format(id_pracownika)
+    cursor.execute(query)
+    dane = cursor.fetchall()
+    for (imie, nazwisko) in dane:
+        print("WITAJ "+imie+" "+nazwisko+"!!!")
 
 
 
--- src/sql/db.sql
+++ src/sql/db.sql
@@ -1,30 +1,204 @@
-create database DB;
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 09 Cze 2020, 16:13
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.2.31
 
-CREATE TABLE IF NOT EXISTS info_o_zwiazku (
-    id_zwiazku int AUTO_INCREMENT,
-    PRIMARY KEY (id_zwiazku),
-    wzor varchar(20),
-    nazwa_zwiazku varchar(20),
-    stan_skupienia varchar(20),
-    uwagi varchar(300),
-    cena_za_gram float(20),
-    obecny_stan_w_magazynie float(20),
-    pH int(5),
-    rodzaj_wiazania_w_zwiazku varchar(20),
-    temp_wrzenia float(20),
-    temp_topnienia float(20),
-    masa_molowa float(20)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE IF NOT EXISTS sprzet_lab(
-	id_sprzetu int AUTO_INCREMENT,
-    PRIMARY KEY (id_sprzetu),
-    nazwa varchar(20),
-    ilosc int(20),
-    uwagi varchar(300)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Baza danych: `laboratorium`
+--
 
-INSERT INTO info_o_zwiazku(wzor, nazwa_zwiazku, stan_skupienia, uwagi, cena_za_gram, obecny_stan_w_magazynie, pH, rodzaj_wiazania_w_zwiazku, temp_wrzenia, temp_topnienia, masa_molowa) VALUES('H2O','woda','ciecz','taka fajna woda', '2','100','0','jakies napewno','100','30','2');
-INSERT INTO sprzet_lab(nazwa, ilosc, uwagi) VALUES ('probowka', '20', 'zadnych uwag');
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `info_o_zwiazku`
+--
+
+CREATE TABLE `info_o_zwiazku` (
+  `id_zwiazku` int(11) NOT NULL,
+  `wzor` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `nazwa_zwiazku` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `stan_skupienia` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `uwagi` varchar(300) COLLATE utf8_polish_ci DEFAULT NULL,
+  `cena_za_gram` float DEFAULT NULL,
+  `obecny_stan_w_magazynie` float DEFAULT NULL,
+  `pH` int(5) DEFAULT NULL,
+  `rodzaj_wiazania_w_zwiazku` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `temp_wrzenia` float DEFAULT NULL,
+  `temp_topnienia` float DEFAULT NULL,
+  `masa_molowa` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `info_o_zwiazku`
+--
+
+INSERT INTO `info_o_zwiazku` (`id_zwiazku`, `wzor`, `nazwa_zwiazku`, `stan_skupienia`, `uwagi`, `cena_za_gram`, `obecny_stan_w_magazynie`, `pH`, `rodzaj_wiazania_w_zwiazku`, `temp_wrzenia`, `temp_topnienia`, `masa_molowa`) VALUES
+(1, 'H2O', 'woda', 'ciecz', 'taka fajna woda', 2, 100, 0, 'jakies napewno', 100, 30, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `odbiorcy_odpadow_chemicznych`
+--
+
+CREATE TABLE `odbiorcy_odpadow_chemicznych` (
+  `nazwa_firmy` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `data_odbioru` date DEFAULT NULL,
+  `cena_za_wiaderko` float DEFAULT NULL,
+  `ilosc_odebranych_wiaderek` int(20) DEFAULT NULL,
+  `id_odbioru` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pracownicy`
+--
+
+CREATE TABLE `pracownicy` (
+  `id_pracownika` int(11) NOT NULL,
+  `imie` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `numer_konta` bigint(20) NOT NULL,
+  `pensja` int(11) NOT NULL,
+  `adres` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `data_zatrudnienia` date NOT NULL,
+  `godzina_rozpoczecia_pracy` tinyint(4) NOT NULL,
+  `godzina_zakonczenia_pracy` tinyint(4) NOT NULL,
+  `stanowisko` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `login` text COLLATE utf8mb4_polish_ci NOT NULL,
+  `haslo` text CHARACTER SET utf32 COLLATE utf32_polish_ci NOT NULL,
+  `email` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `pracownicy`
+--
+
+INSERT INTO `pracownicy` (`id_pracownika`, `imie`, `nazwisko`, `numer_konta`, `pensja`, `adres`, `data_zatrudnienia`, `godzina_rozpoczecia_pracy`, `godzina_zakonczenia_pracy`, `stanowisko`, `login`, `haslo`, `email`, `admin`) VALUES
+(1, 'karol', 'krol', 0, 0, '', '0000-00-00', 0, 0, '', '', '', '', 0),
+(2, 'arrrtur', 'haaamerski', 0, 0, '', '0000-00-00', 0, 0, '', '', '', '', 0),
+(3, 'imie', 'nazwiko', 41241241, 0, 'krakow', '0000-00-00', 0, 0, '', 'admin', 'admin', 'emejl', 1),
+(4, 'a', 'a', 0, 0, 'a', '0000-00-00', 0, 0, '', 'a', 'a', 'a', 0),
+(7, 'b', 'b', 4, 0, 'b', '0000-00-00', 0, 0, '', 'b', 'dada', 'b', 0),
+(8, 'c', 'c', 0, 0, 'c', '0000-00-00', 0, 0, '', 'c', 'c', 'c', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `sprzet_lab`
+--
+
+CREATE TABLE `sprzet_lab` (
+  `id_sprzetu` int(11) NOT NULL,
+  `nazwa` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `ilosc` int(20) DEFAULT NULL,
+  `uwagi` varchar(300) COLLATE utf8_polish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zuzyte_zwiazki`
+--
+
+CREATE TABLE `zuzyte_zwiazki` (
+  `id_pracownika` int(20) DEFAULT NULL,
+  `id_zwiazku` int(20) DEFAULT NULL,
+  `data_zuzycia` date DEFAULT NULL,
+  `ilosc_zuzycia` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `info_o_zwiazku`
+--
+ALTER TABLE `info_o_zwiazku`
+  ADD PRIMARY KEY (`id_zwiazku`);
+
+--
+-- Indeksy dla tabeli `odbiorcy_odpadow_chemicznych`
+--
+ALTER TABLE `odbiorcy_odpadow_chemicznych`
+  ADD PRIMARY KEY (`id_odbioru`);
+
+--
+-- Indeksy dla tabeli `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  ADD PRIMARY KEY (`id_pracownika`);
+
+--
+-- Indeksy dla tabeli `sprzet_lab`
+--
+ALTER TABLE `sprzet_lab`
+  ADD PRIMARY KEY (`id_sprzetu`);
+
+--
+-- Indeksy dla tabeli `zuzyte_zwiazki`
+--
+ALTER TABLE `zuzyte_zwiazki`
+  ADD KEY `id_pracownika` (`id_pracownika`),
+  ADD KEY `id_zwiazku` (`id_zwiazku`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `info_o_zwiazku`
+--
+ALTER TABLE `info_o_zwiazku`
+  MODIFY `id_zwiazku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `odbiorcy_odpadow_chemicznych`
+--
+ALTER TABLE `odbiorcy_odpadow_chemicznych`
+  MODIFY `id_odbioru` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  MODIFY `id_pracownika` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT dla tabeli `sprzet_lab`
+--
+ALTER TABLE `sprzet_lab`
+  MODIFY `id_sprzetu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `zuzyte_zwiazki`
+--
+ALTER TABLE `zuzyte_zwiazki`
+  ADD CONSTRAINT `zuzyte_zwiazki_ibfk_1` FOREIGN KEY (`id_pracownika`) REFERENCES `pracownicy` (`id_pracownika`),
+  ADD CONSTRAINT `zuzyte_zwiazki_ibfk_2` FOREIGN KEY (`id_zwiazku`) REFERENCES `info_o_zwiazku` (`id_zwiazku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
